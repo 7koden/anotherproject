a028431b726bc5c370df6f9edc95e4d43039c708
Kelvin Bongers <kelbongers@live.nl>
2012-08-22 15:07:05 +0200

    Finished name checking function.

--- Axalto Code/Text-Based RPG/libtbrpg/rpg_player.cpp
+++ Axalto Code/Text-Based RPG/libtbrpg/rpg_player.cpp
@@ -1,10 +1,31 @@
 #include "libtbrpg.h"
 
 #include <iostream>
+#include <string>
 
 bool RPG_Player::isNameValid()
 {
-	if(name == "false")
+	if(name.size() > 25)
 		return false;
-	return true; // Placeholder
+	
+	const string charWhitelist = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+	
+	bool charCheck;
+	for(unsigned int i = 0; i < name.size(); i++)
+	{
+		charCheck = false;
+		for(unsigned int j = 0; j < charWhitelist.size(); j++)
+		{
+			if(charWhitelist[j] == name[i])
+			{
+				charCheck = true;
+				break;
+			}
+		}
+		
+		if(!charCheck)
+			return false;
+	}
+	
+	return true;
 }
