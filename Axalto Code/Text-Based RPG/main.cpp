4987cd2e3e5862bb91556f22552b504139743232
Kelvin Bongers <kelbongers@live.nl>
2012-08-22 13:04:21 +0200

    Added loading information output and reformatted the code a bit.

--- Axalto Code/Text-Based RPG/main.cpp
+++ Axalto Code/Text-Based RPG/main.cpp
@@ -7,14 +7,24 @@ using namespace std;
 
 int main()
 {
+	cout << "Loading LIBTBRPG Demo Game..." << endl;
+	
+	// String for general console input
 	string cmd;
-
+	
+	// Loading Room Data
+	cout << " Initializing Room Data... ";
 	RPG_Room* testRoom = new RPG_Room();
-
+	cout << "Done!" << endl;
+	
+	//--------------------------//
+	// Actual start of the game //
+	//--------------------------//
+	
 	cout << "Enter name: ";
 	getline(cin, cmd);
-
+	
 	cout << "Hello, " << cmd << endl;
-
+	
 	return 0;
 }
