63b3f2e0b0e62da212f7c91e3e63f2703c54ad8c
Kelvin Bongers <kelbongers@live.nl>
2012-08-29 08:46:46 +0200

    Commented out a function that isn't compiling on OSX. Temp fix only, should be repaired.

--- Axalto Code/Text-Based RPG/clearscreen.h
+++ Axalto Code/Text-Based RPG/clearscreen.h
@@ -39,15 +39,16 @@ void ClearScreen()
 #else
 #include <unistd.h>
 #include <term.h>
+
 void ClearScreen()
 {
-  if (!cur_term)
-    {
+  /*if (!cur_term)
+  {
     int result;
     setupterm( NULL, STDOUT_FILENO, &result );
     if (result <= 0) return;
-    }
+  }
 	
-  putp( tigetstr( "clear" ) );
+  putp( tigetstr( "clear" ) ); Doesn't work, commented out */
 }
 #endif
